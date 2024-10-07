278a7b62056de6f0871bd72b5ec419efafffbe60
Christy-kwakkel <christy.kwakkel@student.nhlstenden.com>
2024-10-07 15:04:44 +0200

    menu-create

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,29 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+// Prompt the user for calculate of multiplication
+  int input = 0;
+   while (Serial.available() <= 0)
+  {
+    Serial.println("1(calculate sum) or 2(multiplication)? ");
+    while (input  1 < || input > 2)
+    {
+    }
+
+  if (input == 1)
+  {
+    //function calculation
+  }
+
+  if (input == 2)
+  {
+    // multiplication
+  }
 }
 
 // put function definitions here:
