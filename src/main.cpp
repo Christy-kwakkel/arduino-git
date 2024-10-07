6566cd0c0d31faedb99da002a864d043f7affe12
iLootYou <leon.kremer@student.nhlstenden.com>
2024-10-07 12:11:44 +0200

    blink

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,23 @@
 // put function declarations here:
 int myFunction(int, int);
 
+// the setup function runs once when you press reset or power the board
+
+int ledPin = 13;
+
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(ledPin, OUTPUT);
 }
 
+// the loop function runs over and over again forever
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(ledPin, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(100);                      // wait for a second
+  digitalWrite(ledPin, LOW);   // turn the LED off by making the voltage LOW
+  delay(500);                      // wait for a second
 }
+
 
 // put function definitions here:
 int myFunction(int x, int y) {
