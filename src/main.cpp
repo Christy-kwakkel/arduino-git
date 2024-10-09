484b7a5772dc583a0677db091db4562d8f495997
Christy-kwakkel <christy.kwakkel@student.nhlstenden.com>
2024-10-09 15:00:15 +0200

    tekst

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,35 @@
 #include <Arduino.h>
 
-void setup() {
+void setup() 
+{
   Serial.begin(9600);
-
 }
 
+
 void loop() 
+{
 // Prompt the user for calculate of multiplication
   int input = 0;
+
    while (Serial.available() <= 0)
   {
     Serial.println("1(calculate sum) or 2(multiplication)? ");
-    while (input  1 < || input > 2)
-    {
-    }
+    while (input < 1 || input > 2)
+    input = Serial.parseInt();
+  }
 
   if (input == 1)
   {
     //function calculation
+    Serial.println("1");
+    input = 0;
   }
 
   if (input == 2)
   {
     // multiplication
+    Serial.println("1");
+    input = 0;
   }
 }
 
