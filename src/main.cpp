a743fa366fe1751773f2f53e68e79e8e22f27db9
Peter Möller <peter.moller@student.nhlstenden.com>
2024-10-10 14:00:54 +0200

    gave everything its own function and added commenting, general code cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,42 @@
+// Libraries
 #include <Arduino.h>
 
+// Constants
+#define LEDPIN 13
+
+// Global variables
 float number1;
 float number2;
 
-// put function declarations here:
+// Function declaration
 float multiplication(float number1, float number2);
 void getNumbers();
-const int ledPin = 13;
+void blink();
+void menu();
+
 void setup() 
 {
-  Serial.begin(9600);
+  Serial.begin(9600); // Start the Serial monitor
 }
-
 
 void loop() 
 {
-  // digitalWrite(ledPin, HIGH);  // turn the LED on (HIGH is the voltage level)
-  // delay(100);                      // wait for a second
-  // digitalWrite(ledPin, LOW);   // turn the LED off by making the voltage LOW
-  // delay(500); 
+  blink();
+  menu();
+}
 
-// Prompt the user for calculate of multiplication
+void blink()
+{
+  digitalWrite(LEDPIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(100);                      // wait for a second
+  digitalWrite(LEDPIN, LOW);   // turn the LED off by making the voltage LOW
+  delay(500); 
+}
+
+
+void menu()
+{
+  // Prompt the user for calculate of multiplication
   Serial.println("1(calculate sum) or 2(multiplication)? ");
   while (Serial.available() <= 0){}
   int input = Serial.parseInt();
@@ -33,9 +49,9 @@ void loop()
     input = 0;
   }
 
+  // Go to multiplication function and print out the result, resetting all inputs after
   if (input == 2)
   {
-    // multiplication
     float result = multiplication(number1, number2);
     input = 0;
     Serial.println(result);
