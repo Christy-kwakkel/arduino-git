c708f44bea6b38106b99085d0feff3e83efe8119
iLootYou <leon.kremer@student.nhlstenden.com>
2024-10-14 13:18:57 +0200

    zonder delays

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,36 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const int ledPin = LED_BUILTIN;    // the number of the LED pin
+int ledState = LOW;                // ledState used to set the LED
+unsigned long previousMillis = 0;  // will store last time LED was updated
+const long interval = 1000;        // interval at which to blink (milliseconds)
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup() 
+{
+  // set the digital pin as output:
+  pinMode(ledPin, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop() 
+{
+  // if the difference between the current time and last time you blinked the LED is bigger than
+  // the interval at which you want to blink the LED.
+  unsigned long currentMillis = millis();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if (currentMillis - previousMillis >= interval) 
+  {
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
+
+    // if the LED is OFF turn it ON and vice-versa:
+    if (ledState == LOW) 
+    {
+      ledState = HIGH;
+    } else 
+    {
+      ledState = LOW;
+    }
+    // set the LED with the ledState of the variable:
+    digitalWrite(ledPin, ledState);
+  }
 }
